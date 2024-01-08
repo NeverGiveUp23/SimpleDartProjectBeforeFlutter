d959f24981ebee591cf118ea2b2a691b4f933854
felix <felixvjr0@gmail.com>
2024-01-07 19:42:16 -0500

    Fetching books from the google books api.

--- Collections.dart
+++ Collections.dart
@@ -11,25 +11,48 @@ import 'package:http/http.dart' as http;
 // printType: PrintType.books,
 // orderBy: OrderBy.relevance,
 // );
+
+
+// this is the basic fethcing with the url
 const baseUrl = 'https://www.googleapis.com/books/v1/volumes';
 
 Future<void> searchBook() async {
-  final url = Uri.parse("$baseUrl?q=The Lord of the Rings");
+  final url = Uri.parse("$baseUrl?q=A Court of Thorn and Roses");
   final res = await http.get(url);
 
   if(res.statusCode == 200){
     final jsonData = jsonDecode(res.body);
     final items = jsonData['items'] as List;
 
+
     for(final item in items){
       print(item["volumeInfo"]['title']);
+      print(item['volumeInfo']['authors']);
+      print(item['volumeInfo']['description']);
     }
   } else {
     print("Error fetching books");
   }
 }
 
+
+// this is the fetching with the google_books library
+
+Future<void> searchBooksWLibrary(String query) async {
+  final booksApi = GoogleBooksApi();
+  final results = await booksApi.searchBooks(
+      query
+  );
+
+  for( final volume in results){
+    print(volume.volumeInfo.imageLinks);
+  }
+}
+
 void main() async {
-await searchBook();
+print("Loading...");
+var bookData = await Future.delayed(Duration(seconds: 5), () =>
+    searchBooksWLibrary('A Court of Thorns and Roses')) ;
+
 
 }
